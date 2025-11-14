86ae8210b2a27b58cf3dfbb9b675283387838af3
Ant0ni0Castr0 <antoniocastro2008@outlook.com>
2025-11-13 22:16:59 -0500

    Change #2: In Diamond.Java, I managed to figure out how to print a perfect even diamond. I also added a line of code at the beginning of the if statement, where if the number is 1 or less, it won't make a diamond since you cant make a diamond with a length of 1, 0, or negative integers. Also, I deleted the lines of code that would print the middle row, and tweaked the for loop that prints the top rows so then it would also print the middle row as well.

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -5,15 +5,73 @@ public class Diamond {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Diamond example");
     System.out.print("Please enter in a positive integer: ");
+
     int userInput = scanner.nextInt();
 
-    if (userInput %2 == 0) {
+    if (userInput <= 1){
+      System.out.println("We can't make a diamond with that number.");
+    } else if (userInput %2 == 0) {
+      int asterisks = 2;
+      int spaces = userInput-1;
+
+      System.out.println();
+
+      for (int i = spaces; i >= 0; i--){
+        System.out.print(" ");
+      }
+      System.out.print("*");
+      System.out.println();
+      spaces--;
+
+      for (int i = 0; i <= userInput-2; i += 2){
+        int spacesTopRows = spaces;
+        int asterisksTopRows = asterisks;
+
+        while (spacesTopRows > 0){
+          System.out.print(" ");
+          spacesTopRows--;
+        }
+        while (asterisksTopRows > 0){
+          System.out.print(" *");
+          asterisksTopRows--;
+          }
+        System.out.println();
+        asterisks += 2;
+        spaces -= 2;
+      }
+      asterisks -= 4;
+      spaces += 4;
+
+      for (int i = 0; i < userInput-2; i += 2){
+        int spacesBottomRows = spaces;
+        int asterisksBottomRows = asterisks;
+
+        while (spacesBottomRows > 0){
+          System.out.print(" ");
+          spacesBottomRows--;
+        }
+        while (asterisksBottomRows > 0){
+          System.out.print(" *");
+          asterisksBottomRows--;
+          }
+        System.out.println();
+        asterisks -= 2;
+        spaces += 2;
+      }
+      for (int i = spaces; i > 0; i--){
+        System.out.print(" ");
+      }
+      System.out.print("*");
+    } else if (userInput <= 0){
+      System.out.println("We can't make a diamond with that number.");
     } else {
       int asterisks = 1;
       int spaces = (userInput - 1)/2;
-      for (int i = 0; i <= userInput-2; i = i + 2){
+
+      for (int i = 0; i < userInput; i += 2){
         int spacesTopRows = spaces;
         int asterisksTopRows = asterisks;
+
         while (spacesTopRows > 0){
           System.out.print(" ");
           spacesTopRows--;
@@ -22,21 +80,17 @@ public class Diamond {
           System.out.print("*");
           asterisksTopRows--;
           }
-        System.out.println("");
-        asterisks = asterisks + 2;
+        System.out.println();
+        asterisks += 2;
         spaces--;
       }
-      int asterisksMiddleRow = asterisks;
-      while (asterisksMiddleRow > 0) {
-        System.out.print("*");
-        asterisksMiddleRow--;
-      }
-      asterisks = asterisks - 2;
-      spaces++;
-      System.out.println("");
-      for (int i = 0; i <= userInput-2; i = i + 2){
+      asterisks -= 4;
+      spaces += 2;
+
+      for (int i = 0; i <= userInput-2; i += 2){
         int spacesBottomRows = spaces;
         int asterisksBottomRows = asterisks;
+
         while (spacesBottomRows > 0){
           System.out.print(" ");
           spacesBottomRows--;
@@ -45,14 +99,11 @@ public class Diamond {
           System.out.print("*");
           asterisksBottomRows--;
           }
-        System.out.println("");
-        asterisks = asterisks - 2;
+        System.out.println();
+        asterisks -= 2;
         spaces++;
       }
     }
-
-
-
     scanner.close();
   }
 }
