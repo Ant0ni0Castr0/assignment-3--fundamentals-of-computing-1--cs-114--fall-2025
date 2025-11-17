60bf76f4ab992ac510716e89d42ae5b68b48f977
Ant0ni0Castr0 <antoniocastro2008@outlook.com>
2025-11-17 15:13:12 -0500

    Change #3: Created a Matrix class that swaps all numbers except the diagonal row from top right to bottom left, also added other functions in the Matrix class called swap, populateMatrix, printMatrix, and flipMatrix. I then used the Matrix class in the RunMatrix.java program.

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,52 @@
 public class Matrix {
+  private int[][] matrix;
+
+  public Matrix(int size) {
+    matrix = new int[size][size];
+    System.out.println("Your matrix is created with dimensions: " + size + " x "
+                      + size);
+  }
+
+  private void swap(int x1, int y1, int x2, int y2) {
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
+
+  public void populateMatrix() {
+    int value = 1;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        matrix[i][j] = value++;
+      }
+    }
+  }
+
+  public void printMatrix() {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (j == matrix.length - 1 - i) {
+          System.out.print("\u001B[33m" + matrix[i][j] + "\u001B[0m\t");
+        } else {
+          System.out.print(matrix[i][j] + "\t");
+        }
+      }
+      System.out.println();
+    }
+  }
+  public void flipMatrix() {
+    int num = matrix.length;
+
+    for (int i = 0; i < num; i++) {
+      for (int j = 0; j < num - i -1; j++) {
+        int x2 = num - 1 - j;
+        int y2 = num - 1 - i;
+
+        if (j == num - 1 - i || (i == y2 && j == x2)) {
+          continue;
+        }
+        swap(i, j, y2, x2);
+      }
+    }
+  }
 }
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,28 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Matrix example");
+    System.out.print("Please enter in the size of the matrix: ");
+    int size = scanner.nextInt();
+
+    Matrix matrix = new Matrix(size);
+    System.out.println("Printing matrix with defualt values: ");
+    matrix.printMatrix();
+
+    System.out.println("Populating matrix...matrix populated");
+    matrix.populateMatrix();
+
+    System.out.println("Priting Matrix: ");
+    matrix.printMatrix();
+
+    System.out.println("Flipping matrix...matrix flipped");
+    matrix.flipMatrix();
+
+    System.out.println("Printing flipped Matrix: ");
+    matrix.printMatrix();
+
+    scanner.close();
   }
 }
